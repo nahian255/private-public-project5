f843da4c4b548ad5e3bba250d0b97771a0467db6
nahian255 <nahimn253@gmail.com>
2022-11-11 12:07:44 +0600

    redirects file add

--- src/Component/Statistics.jsx
+++ src/Component/Statistics.jsx
@@ -4,7 +4,6 @@ const Statistics = () => {
     return (
         <div>
             <h1 className='text-6xl'>Statistics Part</h1>
-            <h1>jsalfkjs fjsdlkjf sdjflsdj fsdlkjfasdjf sdlkfjsdlfj </h1>
         </div>
     );
 };
