67abe93eee3192e25eb0e52990976b7a2197e58b
nahian255 <nahimn253@gmail.com>
2022-11-11 12:07:19 +0600

    file add

--- src/Component/Statistics.jsx
+++ src/Component/Statistics.jsx
@@ -4,6 +4,7 @@ const Statistics = () => {
     return (
         <div>
             <h1 className='text-6xl'>Statistics Part</h1>
+            <h1>jsalfkjs fjsdlkjf sdjflsdj fsdlkjfasdjf sdlkfjsdlfj </h1>
         </div>
     );
 };
